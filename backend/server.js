0f4896c1c52fdd04b2ac4e0f21f2a2dd8b666264
Mahrukh Sajjad <mahrukhsajjad758@gmail.com>
2025-08-23 11:32:48 -0700

    Committed

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000; // ✅ use dynamic port for deployment
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
